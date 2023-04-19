6689b575ffc0252e932502336e0fffea3351b898
Kabelo Mashishi <kabelo.mashishi@finflx.com>
2023-04-19 11:35:21 +0200

    added auth to application

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "reactamplifyapp7ee5d90e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import "@aws-amplify/ui-react/styles.css";
+import {withAuthenticator,Button,Heading,Image,View,Card} from "@aws-amplify/ui-react";
 
-function App() {
+
+function App({ signOut }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h3>App Build in Progress</h3>
-        </p>
-      </header>
-    </div>
+    <View className="App">
+      <Card>
+      <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>Auth enabled!</Heading>
+      </Card>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
